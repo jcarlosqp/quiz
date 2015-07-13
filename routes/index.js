32c55478a9fc9f93bc6c6019703fc9e5194ae695
jcarlosqp <okeyjc@hotmail.com>
2015-07-12 22:37:51 -0500

    QUIZ- Ejercicio realizado

--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,7 @@ router.get('/', function(req, res) {
 });
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
-
+router.get('/author', function(req, res) {
+  res.render('quizes/author',{author:'Juan Carlos Quispe Pampa'});
+});
 module.exports = router;
