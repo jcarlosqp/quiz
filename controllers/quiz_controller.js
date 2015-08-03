fabdb3615f5fac42de3dc4a8eeab94308a1ac29c
Juan Carlos Quispe Pampa <jcarlos25@live.com>
2015-08-02 19:34:26 -0500

    Ejercicio P2P Mod 8 sin Base Datos
    Avance del modulo 8 y el ejercicio solicitado

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -14,3 +14,101 @@ exports.answer=function(req,res){
 exports.author=function(req,res){
 	res.render('quizes/author',{author:'Juan Carlos Quispe Pampa'});
 };
+
+
+
+
+// GET /quizes
+exports.index = function (req, res) {
+    var buscar = req.query.search || '';
+
+    if (buscar === '') {
+        models.Quiz.findAll().then(
+			function (quizes) {
+			    res.render('quizes/index', { quizes: quizes, errors: [] });
+
+			}
+
+		);
+
+    } else {
+
+        buscar = buscar.replace(/ /g, '%');
+
+        models.Quiz.findAll({ where: ["pregunta like ?", '%' + buscar + '%'], order: 'pregunta' }).then(
+			function (quizes) {
+			    res.render('quizes/index', { quizes: quizes, errors: [] });
+
+			}
+
+		);
+
+    }
+
+};
+
+// GET /quizes/:id
+exports.show = function (req, res) {
+    models.Quiz.find(req.params.quizId).then(
+		function (quiz) {
+		    res.render('quizes/show', { quiz: quiz, errors: [] });
+
+		}
+
+	);
+
+};
+
+exports.new = function (req, res) {
+    var quiz = models.Quiz.build(
+        { pregunta: "Pregunta", respuesta: "Respuesta", tema:"Tema" }
+        );
+    res.render('quizes/new', { quiz: quiz });
+};
+
+exports.create = function (req, res) {
+    var quiz = models.Quiz.build(req.body.quiz);
+
+    quiz.quiz({ fields: ["pregunta", "respuesta","tema"] }).then(function () {
+        res.redirect('/quizes');
+    })
+    
+};
+
+// GET /quizes/:id/edit
+exports.edit = function (req, res) {
+    var quiz = req.quiz; 
+
+    res.render('quizes/edit', { quiz: quiz, errors: [] });
+
+};
+
+exports.update = function (req, res) {
+
+    req.quiz.pregunta = req.body.quiz.pregunta;
+    req.quiz.respuesta = req.body.quiz.respuesta;
+    req.quiz.tema = req.body.quiz.tema;
+
+    var hay_error = req.quiz.validate();
+
+    if (hay_error) {
+
+        var errs = [];
+        for (var i in hay_error) {
+            errs = errs.concat(hay_error[i]);
+
+        }
+
+        res.render('quizes/edit', { quiz: req.quiz, errors: errs });
+
+    } else {
+
+        req.quiz.save({ fields: ["pregunta", "respuesta", "tema"] }).then(
+
+			function () { res.redirect('/quizes'); }
+
+		);
+
+    }
+
+};
--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,21 @@ router.get('/quizes/answer', quizController.answer);
 router.get('/author', function(req, res) {
   res.render('quizes/author',{author:'Juan Carlos Quispe Pampa'});
 });
+
+
+
+
+router.get('/quizes/new', quizController.new);
+router.post('/quizes/create', quizController.create);
+
+// Definición de las rutas de /quizes
+router.get('/quizes', quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+
+router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
+router.put('/quizes/:quizId(\\d+)', quizController.update);
+
+router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
+
 module.exports = router;
